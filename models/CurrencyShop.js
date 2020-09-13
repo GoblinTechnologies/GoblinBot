eeafacb848f4943f14aa0fc8882eae6cce87ede1
Steve <mythotical@goblin-technologies.com>
2020-09-12 21:31:59 -0500

    Added models directory and files

---
+++ models/CurrencyShop.js
@@ -0,0 +1,16 @@
+/*jshint esversion: 6 */
+
+module.exports = (sequelize, DataTypes) => {
+	return sequelize.define('currency_shop', {
+		name: {
+			type: DataTypes.STRING,
+			unique: true,
+		},
+		cost: {
+			type: DataTypes.INTEGER,
+			allowNull: false,
+		},
+	}, {
+		timestamps: false,
+	});
+};
---
+++ models/Users.js
@@ -0,0 +1,17 @@
+/*jshint esversion: 6 */
+
+module.exports = (sequelize, DataTypes) => {
+	return sequelize.define('users', {
+		user_id: {
+			type: DataTypes.STRING,
+			primaryKey: true,
+		},
+		balance: {
+			type: DataTypes.INTEGER,
+			defaultValue: 0,
+			allowNull: false,
+		},
+	}, {
+		timestamps: false,
+	});
+};
